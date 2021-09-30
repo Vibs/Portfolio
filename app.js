afbbdd0741bdb60e62ecf681e51c2e7a627d7e7f
Vibs <vibej@hotmail.com>
2021-09-30 16:06:57 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// imports express and saves in variabel
+const express = require("express");
+// instatiates express
+const app = express();
+// 
+app.use(express.json());
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get("/:language", (req, res) => {
+    const language = req.params.language;
+    language === "da" || language === "en" 
+        ? res.sendFile(__dirname + `/public/html/${language}/index.html`) 
+        : res.sendStatus(404);
+});
+
+
+
+
+
+
+
+
+
+
+
+app.listen(8080, (error) => {
+    error ? console.log("Server could not start up:", error) : console.log("Starting server on port", 8080);;
+});
